8bae895a9b98fd859ca31e72d8954b6e2799fe8a
Tim Destan <tim@destan.name>
2016-08-27 00:11:29 -0400

    Add vars to Expr.

--- math.rs
+++ math.rs
@@ -2,27 +2,46 @@
 #[derive(Debug)]
 enum Expr {
   Lit(i32),
+  Var(String),
   Add(Box<Expr>, Box<Expr>),
   Mult(Box<Expr>, Box<Expr>),
 }
 
 use Expr::*;
 
-fn simplify(e: &Expr) -> i32 {
+fn simplify(e: &Expr) -> Expr {
   match *e {
-    Lit(l) => l,
-    Add(ref l, ref r) => simplify(l) + simplify(r),
-    Mult(ref l, ref r) => simplify(l) * simplify(r)
+    Lit(l) => Lit(l),
+    Var(ref s) => Var(s.clone()),
+    Add(ref l, ref r) => {
+      let lhs = simplify(l);
+      let rhs = simplify(r);
+      match (lhs, rhs) {
+        (Lit(l), Lit(r)) => Lit(l + r),
+        (l, r) => Add(Box::new(l), Box::new(r))
+      }
+    },
+    Mult(ref l, ref r) => {
+      let lhs = simplify(l);
+      let rhs = simplify(r);
+      match (lhs, rhs) {
+        (Lit(l), Lit(r)) => Lit(l * r),
+        (l, r) => Mult(Box::new(l), Box::new(r))
+      }
+    }
   }
 }
 
+fn test_simplify(tree: Expr) {
+  println!("simplify({:?}) = {:?}", tree, simplify(&tree));
+}
+
 fn main() {
-  let tree = Add(
+  test_simplify(Add(
     Box::new(Mult(
       Box::new(Lit(4)),
       Box::new(Lit(5))
     )),
-    Box::new(Lit(7))
-  );
-  println!("simplify({:?}) = {}", tree, simplify(&tree));
+    Box::new(Var("x".to_string()))
+  ));
 }
